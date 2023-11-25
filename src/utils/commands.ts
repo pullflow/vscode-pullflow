ba6918b275a33bd519029de18294b5dd7f79b269
Syeda Zainab <83520846+srzainab@users.noreply.github.com>
2023-11-25 14:27:11 +0400

    added dev mode renaming in commands.ts

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -1,8 +1,16 @@
-export const Command = {
+import { isDevelopment } from './appConfig'
+
+const Commands = {
   signIn: 'pullflow.signIn',
   activePullRequests: 'pullflow.active-pull-requests',
   signOut: 'pullflow.signOut',
   reconnect: 'pullflow.reconnect',
   toggleFlowState: 'pullflow.toggle-flow-state',
   welcomeView: 'pullflow.welcome-view',
+}
+
+export const Command = (key: keyof typeof Commands) => {
+  return isDevelopment()
+    ? Commands[key].replace('pullflow', 'pullflow-dev')
+    : Commands[key]
 }
