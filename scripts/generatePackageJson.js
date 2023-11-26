e59d11e816e7ebdd7192e9a8ff5d9fe3a4441ecd
Syeda Zainab <83520846+srzainab@users.noreply.github.com>
2023-11-26 11:25:59 +0400

    added husky hooks and generatePackageJson script

---
+++ scripts/generatePackageJson.js
@@ -0,0 +1,63 @@
+const fs = require('fs')
+
+const readFile = async () => {
+  const packageContents = await fs.promises.readFile('package.json', {
+    encoding: 'utf-8',
+  })
+
+  return JSON.parse(packageContents)
+}
+
+const writeFile = async (jsonData) => {
+  await fs.writeFileSync('package.json', JSON.stringify(jsonData, null, 2))
+}
+
+const setupDev = async () => {
+  console.log(`👩‍💻 Generating for development`)
+  const jsonPackageContents = await readFile()
+  if (jsonPackageContents.name.includes('-dev')) {
+    console.log(`\n⚠️ Looks like package.json is already setup for development`)
+    return
+  }
+  jsonPackageContents.name = appendDev(jsonPackageContents.name)
+  jsonPackageContents.displayName = appendDev(jsonPackageContents.displayName)
+  jsonPackageContents.contributes.commands =
+    jsonPackageContents.contributes.commands.map((obj) => ({
+      ...obj,
+      command: appendDev(obj.command),
+      title: appendDev(obj.title),
+    }))
+
+  jsonPackageContents.contributes.keybindings =
+    jsonPackageContents.contributes.keybindings.map((binding) => ({
+      ...binding,
+      command: appendDev(binding.command),
+    }))
+  await writeFile(jsonPackageContents)
+  console.log(`\n✓ package.json updated for development`)
+}
+
+const revertDev = async () => {
+  console.log(`🤞 Generating for production\n`)
+  const jsonData = await readFile()
+  jsonData.name = removeDev(jsonData.name)
+  jsonData.displayName = removeDev(jsonData.displayName)
+  jsonData.contributes.commands = jsonData.contributes.commands.map((obj) => ({
+    ...obj,
+    command: removeDev(obj.command),
+    title: removeDev(obj.title),
+  }))
+  jsonData.contributes.keybindings = jsonData.contributes.keybindings.map(
+    (binding) => ({
+      ...binding,
+      command: removeDev(binding.command),
+    })
+  )
+  await writeFile(jsonData)
+  console.log(`✓ package.json updated for production`)
+}
+
+const appendDev = (value) => value.replace(/pullflow/i, '$&-dev')
+const removeDev = (value) => value.replace('-dev', '')
+
+process.argv[2] === 'development' ? setupDev() : revertDev()
